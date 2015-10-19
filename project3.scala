7de59097b94adefc878025e9df0d7c77ce29d531
sharath <sharuu123@gmail.com>
2015-10-18 21:36:21 -0400

    code for fingertable

--- project3.scala
+++ project3.scala
@@ -15,10 +15,13 @@ object project3 {
 
 		case class Initialize()
 		case class AddNextNode(sender: String)
-		case class Join(nodeId: String) 
+		case class Join() 
 		case class SearchPreAndSucc(key: BigInt, sender: String)
 		case class FoundPreAndSucc(pre: String, succ: String)
 		case class UpdatePredecessor(update: String)
+		case class InitFingertable(i: Int)
+		case class SearchOwner(key: BigInt, i: Int, sender: String) 
+		case class FoundOwner(owner: String, i:Int)
 
 		println("project3 - Chord DHT")
 		class Master(numOfNodes: Int, numOfRequests: Int) extends Actor{
@@ -89,20 +92,44 @@ object project3 {
 
 				case UpdatePredecessor(update: String) =>
 					predecessor = update
-					sender ! InitializeFingertable(1)
-					masterRef ! AddNextNode(myID)
+					sender ! InitFingertable(1)
+					
+				case InitFingertable(i: Int) =>
+					if(i<160){
+						var key: BigInt = BigInt(convert2Hashvalue(myID,i))
+						if(key > (sha1(myID)) || key < sha1(fingertable(0)(1))){
+							fingertable(i)(0) = convert2Hashvalue(myID,i)
+							fingertable(i)(1) = fingertable(0)(1)
+							self ! InitFingertable(i+1)
+						} else {
+							context.actorSelection("../" + friend) ! SearchOwner(BigInt(convert2Hashvalue(myID,i)),i,myID)
+						}
+					} else {
+						masterRef ! AddNextNode(myID)
+					}
 
-				case InitializeFingertable(i: Int) =>
-					context.actorSelection("../" + friend) ! SearchOwner(BigInt(convert2Hashvalue(myID,i))
+				case SearchOwner(key: BigInt, i: Int, sender: String) =>
+					if(key > (sha1(myID)) || key < sha1(fingertable(0)(1))){
+						context.actorSelection("../"+sender) ! FoundOwner(myID,i)
+					} else {
+						context.actorSelection("../"+fingertable(0)(1)) ! SearchOwner(key,i,sender)
+					}
 
-				case SearchOwner(key: BigInt) =>
-				
-
+				case FoundOwner(owner: String, i:Int) =>
+					fingertable(i)(0) = convert2Hashvalue(myID,i)
+					fingertable(i)(1) = owner
+					if(160>i+1){
+						context.actorSelection("../" + friend) ! SearchOwner(BigInt(convert2Hashvalue(myID,i+1)),i+1,myID)
+					} else {
+						masterRef ! AddNextNode(myID)
+					}
 			}
 		}
 
 		def convert2Hashvalue(id: String, i:Int): String = {
-			(sha1(id) + BigInt(((pow(2,i)).toInt).toString)).toString
+			var key: BigInt = (sha1(id) + BigInt(((pow(2,i)).toInt).toString))
+			key = key.mod(BigInt(((pow(2,i)).toInt).toString))
+			key.toString
 		}
 
 		def sha1(s: String): BigInt = {
@@ -128,7 +155,7 @@ object project3 {
 
 		// }
 
-		if(args.size<2){
+		if(args.size!=2){
 			println("Enter valid number of inputs!!")
 		} else {
 			val system = ActorSystem("MasterSystem")
